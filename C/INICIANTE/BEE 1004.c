8ca4db6f477ebb949a44b2f283a3e6ed0959ecbc
Theo Nasser <105657127+panabue@users.noreply.github.com>
2022-08-26 19:42:32 -0400

    Create BEE 1004.c

---
+++ C/INICIANTE/BEE 1004.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    int x, y;
+    scanf("%d", &x);
+    scanf("%d", &y);
+    
+    int produto = x * y;
+    printf("PROD = %d\n", produto);
+
+    return 0;
+}
