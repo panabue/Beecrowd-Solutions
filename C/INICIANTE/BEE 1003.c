adc1e152f6ff22852c90b45f895b24ddb4d67d43
Theo Nasser <105657127+panabue@users.noreply.github.com>
2022-08-26 19:37:59 -0400

    Create BEE 1003.c

---
+++ C/INICIANTE/BEE 1003.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    int x, y;
+    scanf("%d",&x);
+    scanf("%d",&y);
+    
+    int soma = x + y;
+    printf("SOMA = %d\n", soma);
+ 
+    return 0;
+}
