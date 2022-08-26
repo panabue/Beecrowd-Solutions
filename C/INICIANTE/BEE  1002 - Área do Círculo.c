2f4d19df58cbfc0fb484f922e7ff648b534e14a1
Theo Nasser <105657127+panabue@users.noreply.github.com>
2022-08-25 21:47:54 -0400

    Create BEE  1002 - Área do Círculo.c

---
+++ C/INICIANTE/BEE  1002 - Área do Círculo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    double raio;
+    double pi=3.14159;
+    scanf("%lf",&raio);
+    double area=pi*raio*raio;
+    printf("A=%.4lf\n", area);
+ 
+    return 0;
+}
