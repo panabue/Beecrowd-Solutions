4a30a45f724b77748e029d39e41d867c25314ffa
Theo Nasser <105657127+panabue@users.noreply.github.com>
2022-08-26 22:36:09 -0400

    Create BEE 1006.c

---
+++ C/INICIANTE/BEE 1006.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double A, B, C;
+    scanf("%lf", &A);
+    scanf("%lf", &B);
+    scanf("%lf", &C);
+    
+    double media = (A*2 + B*3 + C*5)/10;
+    printf("MEDIA = %.1lf\n", media);
+    
+ 
+    return 0;
+}
