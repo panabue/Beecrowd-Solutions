b5a5f8cb93f2ea77ba601ef1d0ac1fc2c77c87fb
Theo Nasser <105657127+panabue@users.noreply.github.com>
2022-09-15 10:31:21 -0400

    Create BEE 1037.c

---
+++ C/INICIANTE/BEE 1037.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    double x;
+    scanf("%lf\n", &x);
+    
+    if(0<=x && x<=25){
+        printf("Intervalo [0,25]\n");
+    }
+    else if(25<x && x<=50){
+        printf("Intervalo (25,50]\n");
+    }
+    else if(50<x && x<=75){
+        printf("Intervalo (50,75]\n");
+    }
+    else if(75<x && x<=100){
+        printf("Intervalo (75,100]\n");
+    }
+    else{
+        printf("Fora de intervalo\n");
+    }
+    
+    return 0;
+}
