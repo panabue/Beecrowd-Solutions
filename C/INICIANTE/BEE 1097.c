71435e546f6b2d2e2de8ad29215bb7e1880e98c8
Theo Nasser <105657127+panabue@users.noreply.github.com>
2022-12-03 22:53:21 -0400

    Create BEE 1097.c

---
+++ C/INICIANTE/BEE 1097.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+ 
+int main() {
+    
+    int i, x, y, z;
+    x=1;
+    y=7;
+    z=0;
+    
+    for(i=1; i<=15; i++){
+        if(z==3){
+            x = 3;
+            y = 9;
+        }
+        if(z==6){
+            x = 5;
+            y = 11;
+        }
+        if(z==9){
+            x = 7;
+            y = 13;
+        }
+        if(z==12){
+            x = 9;
+            y = 15;
+        }
+        printf("I=%d J=%d\n", x, y);
+        y--;
+        z++;
+    }
+    
+    return 0;
+}
