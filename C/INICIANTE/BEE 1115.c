5630f35734257cf5ab17106274676e67a5eaa5d3
Theo Nasser <105657127+panabue@users.noreply.github.com>
2022-12-04 14:22:14 -0400

    Create BEE 1115.c

---
+++ C/INICIANTE/BEE 1115.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int x, y;
+    
+    while(1){
+        scanf("%d %d\n", &x, &y);
+        if(x==0 || y==0){
+            break;
+        }
+        if(x>0 && y>0){
+            printf("primeiro\n");
+        }
+        if(x<0 && y>0){
+            printf("segundo\n");
+        }
+        if(x<0 && y<0){
+            printf("terceiro\n");
+        }
+        if(x>0 && y<0){
+            printf("quarto\n");
+        }
+    }
+ 
+    return 0;
+}
